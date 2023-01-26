461c73c9f22f16ebcec3d43b7d10672675081b37
Alejandro Gomez Fernandez <alegomez@car8s2.42barcelona.com>
2023-01-26 19:34:18 +0100

    Es una prueba simplemente, no es ni un acercamiento

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,11 +6,39 @@
 /*   By: alegomez <alegomez@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 18:24:19 by alegomez          #+#    #+#             */
-/*   Updated: 2023/01/26 18:24:33 by alegomez         ###   ########.fr       */
+/*   Updated: 2023/01/26 19:30:26 by alegomez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
 char	*get_next_line(int fd)
 {
+	static void*	chartxt;
+	int	i;
 
+	i = 0;
+	/*
+	if(!fd)
+		return (0);
+	*/
+	read(fd, chartxt, 100);
+
+	printf("%s", (char*)chartxt);
+/*
+	while()
+	{
+		printf("%d", texto);
+		texto++;
+	}
+*/
+	return ("ey");
+}
+
+int	main(void)
+{
+	int fd = open("../letra.txt", O_RDWR);
+	get_next_line(fd);
 }
